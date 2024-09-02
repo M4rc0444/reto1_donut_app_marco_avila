18543266f522a71bfdbd79dfabf0a6b764c856fd
Marco Avila <marcouavp@gmail.com>
2024-09-02 14:01:58 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_marco_avila
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_marco_avila/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_marco_avila
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
