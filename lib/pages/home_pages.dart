88078aa593f566e4bf1d3450d8abdedb52671ec8
Marco Avila <marcouavp@gmail.com>
2024-09-02 15:41:51 -0600

    iniciando AppBar

---
+++ lib/pages/home_pages.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+  const HomePages({super.key});
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.grey[700],
+        ),
+      ),
+    );
+  }
+}
