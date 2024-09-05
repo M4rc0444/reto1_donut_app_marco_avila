17d204d53159703487a0404af5b838cdaa7769c3
Marco Avila <marcouavp@gmail.com>
2024-09-05 15:21:40 -0600

    añadir_texto

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -13,7 +13,46 @@ class _HomePagesState extends State<HomePages> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(
+            icon: Icon(Icons.menu, color: Colors.grey[800], size: 36),
+            onPressed: () {
+              print("hola mundo");
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(
+              Icons.person,
+              color: Colors.grey[800],
+              size: 36,
+            ),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          //Texto I want to eat
+          Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text("Eat",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline)),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
