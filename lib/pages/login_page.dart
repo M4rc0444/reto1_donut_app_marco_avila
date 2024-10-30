32c77ffb37c995f693a5b798b29a1173ced63114
M4rc0444 <marcouavp@gmail.com>
2024-10-29 21:53:55 -0600

    corregido_parte_todos

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,232 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:reto1_donut_app_marco_avila/pages/forgot_password_page.dart';
+// import 'package:reto1_donut_app_marco_avila/pages/registrarse.dart';
+// import 'home_pages.dart';
+
+// class LoginPage extends StatefulWidget {
+//   const LoginPage({super.key});
+
+//   @override
+//   LoginPageState createState() => LoginPageState();
+// }
+
+// class LoginPageState extends State<LoginPage> {
+//   final TextEditingController _usernameController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   void _login() async {
+//     try {
+//       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+//         email: _usernameController.text,
+//         password: _passwordController.text,
+//       );
+
+//       if (userCredential.user?.emailVerified == true) {
+//         Navigator.pushReplacement(
+//           context,
+//           MaterialPageRoute(builder: (context) => const HomePage()),
+//         );
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           const SnackBar(
+//               content: Text('Por favor verifica tu correo electrónico')),
+//         );
+//       }
+//     } on FirebaseAuthException catch (e) {
+//       String message = 'Error desconocido';
+//       if (e.code == 'user-not-found') {
+//         message = 'No hay ningún usuario registrado con este correo.';
+//       } else if (e.code == 'wrong-password') {
+//         message = 'Contraseña incorrecta.';
+//       }
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         SnackBar(content: Text(message)),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.transparent,
+//       appBar: AppBar(
+//         backgroundColor: Colors.pink,
+//       ),
+//       body: LayoutBuilder(
+//         builder: (context, constraints) {
+//           return Container(
+//             decoration: const BoxDecoration(
+//               gradient: LinearGradient(
+//                 colors: [Colors.pink, Colors.pink],
+//                 begin: Alignment.topCenter,
+//                 end: Alignment.bottomCenter,
+//               ),
+//             ),
+//             child: Center(
+//               child: SingleChildScrollView(
+//                 padding: EdgeInsets.symmetric(
+//                   horizontal: constraints.maxWidth * 0.1,
+//                 ),
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     const Text(
+//                       "Bienvenido,",
+//                       style: TextStyle(
+//                         fontSize: 32,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     const SizedBox(height: 10),
+//                     const Text(
+//                       "inicia sesión para continuar",
+//                       style: TextStyle(
+//                         fontSize: 25,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     SizedBox(height: constraints.maxHeight * 0.05),
+//                     Image.asset("lib/images/logo2.png",
+//                         width: constraints.maxWidth * 0.5),
+//                     SizedBox(height: constraints.maxHeight * 0.03),
+//                     Container(
+//                       padding: EdgeInsets.all(constraints.maxWidth * 0.05),
+//                       decoration: BoxDecoration(
+//                         color: Colors.white,
+//                         borderRadius: BorderRadius.circular(15.0),
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.black.withOpacity(0.1),
+//                             spreadRadius: 5,
+//                             blurRadius: 10,
+//                             offset: const Offset(0, 5),
+//                           ),
+//                         ],
+//                       ),
+//                       child: Column(
+//                         children: [
+//                           Container(
+//                             padding: EdgeInsets.symmetric(
+//                               horizontal: constraints.maxWidth * 0.02,
+//                               vertical: constraints.maxHeight * 0.01,
+//                             ),
+//                             decoration: BoxDecoration(
+//                               borderRadius: BorderRadius.circular(8),
+//                               color: Colors.grey.withOpacity(.2),
+//                             ),
+//                             child: TextFormField(
+//                               controller: _usernameController,
+//                               decoration: const InputDecoration(
+//                                 icon: Icon(Icons.person),
+//                                 border: InputBorder.none,
+//                                 hintText: "Correo Electrónico",
+//                               ),
+//                             ),
+//                           ),
+//                           SizedBox(height: constraints.maxHeight * 0.02),
+//                           Container(
+//                             padding: EdgeInsets.symmetric(
+//                               horizontal: constraints.maxWidth * 0.02,
+//                               vertical: constraints.maxHeight * 0.01,
+//                             ),
+//                             decoration: BoxDecoration(
+//                               borderRadius: BorderRadius.circular(8),
+//                               color: Colors.grey.withOpacity(.2),
+//                             ),
+//                             child: TextFormField(
+//                               controller: _passwordController,
+//                               obscureText: true,
+//                               decoration: const InputDecoration(
+//                                 icon: Icon(Icons.lock),
+//                                 border: InputBorder.none,
+//                                 hintText: "Contraseña",
+//                               ),
+//                             ),
+//                           ),
+//                           SizedBox(height: constraints.maxHeight * 0.02),
+//                           ElevatedButton(
+//                             onPressed: _login,
+//                             style: ElevatedButton.styleFrom(
+//                               foregroundColor: Colors.white,
+//                               backgroundColor: Colors.pink[400],
+//                               padding: EdgeInsets.symmetric(
+//                                 horizontal: constraints.maxWidth * 0.25,
+//                                 vertical: constraints.maxHeight * 0.025,
+//                               ),
+//                               shape: RoundedRectangleBorder(
+//                                 borderRadius: BorderRadius.circular(90.0),
+//                               ),
+//                             ),
+//                             child: const Text(
+//                               'Iniciar sesión',
+//                               style: TextStyle(
+//                                 fontSize: 18,
+//                                 fontWeight: FontWeight.bold,
+//                                 color: Colors.white,
+//                               ),
+//                             ),
+//                           ),
+//                           TextButton(
+//                             onPressed: () {
+//                               Navigator.push(
+//                                 context,
+//                                 MaterialPageRoute(
+//                                   builder: (context) =>
+//                                       const ForgotPasswordPage(),
+//                                 ),
+//                               );
+//                             },
+//                             child: const Text(
+//                               '¿Olvidaste tu contraseña?',
+//                               style: TextStyle(color: Colors.blue),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                     SizedBox(height: constraints.maxHeight * 0.02),
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         const Text(
+//                           "¿Aún no tienes una cuenta?",
+//                           style: TextStyle(
+//                             fontSize: 17,
+//                             color: Colors.white,
+//                           ),
+//                         ),
+//                         TextButton(
+//                           onPressed: () {
+//                             Navigator.push(
+//                               context,
+//                               MaterialPageRoute(
+//                                   builder: (context) => const Registrate()),
+//                             );
+//                           },
+//                           child: const Text(
+//                             "Regístrate",
+//                             style: TextStyle(
+//                               fontSize: 17,
+//                               color: Colors.white,
+//                               fontWeight: FontWeight.bold,
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -316,7 +545,7 @@ class LoginPageState extends State<LoginPage> {
             ),
           );
         },
-      ),
-    );
-  }
+      ),
+    );
+  }
 }
